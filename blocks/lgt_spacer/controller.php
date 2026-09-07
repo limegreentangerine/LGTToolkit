b587fba9546ba34372f1b5d139817b81d75d9b03
Lee Jones <prolific.jones82@gmail.com>
2026-09-07 10:06:21 +0100

    added lgt spacer block

---
+++ blocks/lgt_spacer/controller.php
@@ -0,0 +1,30 @@
+<?php
+namespace Concrete\Package\LgtToolkit\Block\LgtSpacer;
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+use Page;
+use Concrete\Core\Block\BlockController;
+
+class Controller extends BlockController
+{
+    protected $btTable = 'btLgtSpacer';
+    protected $btDefaultSet = 'basic';
+    protected $btInterfaceWidth = 800;
+    protected $btInterfaceHeight = 600;
+
+    public function getBlockTypeName()
+    {
+        return t('Spacer');
+    }
+
+    public function getBlockTypeDescription()
+    {
+        return t('Create space between blocks on a page.');
+    }
+
+    public function view()
+    {
+        $this->set('c', Page::getCurrentPage());
+    }
+}
