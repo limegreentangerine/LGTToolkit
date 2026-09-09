be2e5fc198e1eba7a5f0d656950d217f4f1eed51
Lee Jones <prolific.jones82@gmail.com>
2026-09-09 10:25:39 +0100

    Some issues with saving custom attributes

--- attributes/lgt_colour_picker/form.php
+++ attributes/lgt_colour_picker/form.php
@@ -1,5 +1,7 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.'); ?>
 
 <div class="form-group">
-    <?php echo $form->color($this->controller->field('value'), $value ?? '') ?>
+    <?php echo $form->color($this->controller->field('value'), $value ?? '', [
+        'style' => 'height:45px',
+    ]) ?>
 </div>
