d6bc5746360d317051058b7c621bcfe7d8daefed
Lee Jones <prolific.jones82@gmail.com>
2026-09-22 13:15:08 +0100

    bumped version

--- controller.php
+++ controller.php
@@ -44,7 +44,7 @@ class Controller extends PackageController
      *
      * @var string
      */
-    protected $pkgVersion = '0.0.1';
+    protected $pkgVersion = '0.0.3';
 
     /**
      * The minimum Concrete version compatible with the package.
